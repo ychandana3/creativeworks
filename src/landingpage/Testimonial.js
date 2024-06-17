dc6f6912e6461a58fa4b30747d061431717d3989
Yamini Chandana <yaminichandana9@gmail.com>
2024-06-17 18:58:23 +0530

    Changed some texts
    Changes in testimonial data

--- src/landingpage/Testimonial.js
+++ src/landingpage/Testimonial.js
@@ -1,12 +1,25 @@
 import React from "react";
 
-const Testimonial = ({ avatar, name, country, comment, rating = 4.5 }) => {
+const Testimonial = ({
+  avatar,
+  name,
+  country,
+  comment,
+  rating = 4.5,
+  isNextPressed,
+  isPreviousPressed,
+}) => {
   return (
-    <div className=" h-auto md:mr-8 border border-gray-500 bg-white rounded-lg shadow-md p-4 mr-16 flex flex-col transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300 ">
+    <div
+      className={`${
+        isNextPressed != 0 ? "translate-x-" : ""
+      } h-auto md:mr-8 border border-gray-500 bg-white rounded-lg shadow-md p-4 mr-16 flex flex-col transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300`}
+      style={{ transform: `translateX(${isNextPressed * 10}px)` }}
+    >
       <div className="flex items-center justify-between">
         <div className="flex">
           <img
-            src={process.env.PUBLIC_URL + '/avatar.png'}
+            src={process.env.PUBLIC_URL + "/avatar.png"}
             alt={name}
             className="w-16 h-16 rounded-full mr-4"
           />
@@ -15,9 +28,7 @@ const Testimonial = ({ avatar, name, country, comment, rating = 4.5 }) => {
             <p className="text-gray-600">{country}</p>
           </div>
         </div>
-        <div className="">
-          {rating} ⭐
-        </div>
+        <div className="">{rating} ⭐</div>
       </div>
       <div className="mt-2">
         <p className="text-gray-800">{comment}</p>
--- src/landingpage/Testimonials.js
+++ src/landingpage/Testimonials.js
@@ -1,97 +1,120 @@
 import React, { useState } from "react";
 import Testimonial from "./Testimonial";
 
-
 const Testimonials = () => {
   const [startIndex, setStartIndex] = useState(0);
+  const [isNextPressed, setIsNextPressed] = useState(0);
+  const [isPreviousPressed, setIsPreviousPressed] = useState(false);
+  console.log(isNextPressed);
   const testimonials = [
     {
       id: 1,
       avatar: "https://via.placeholder.com/150",
-      name: "John Doe",
-      country: "United States",
+      name: "Yessica Christy",
+      country: "Shanxi, China",
       comment:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "I like it because I like to travel far and still can connect with people around world.",
     },
     {
       id: 2,
       avatar: "https://via.placeholder.com/150",
-      name: "Jane Smith",
-      country: "Canada",
+      name: "Viezh Robert",
+      country: "Warsaw, Poland",
+      comment:
+        "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. CreativeWorks always the best",
+    },
+    {
+      id: 3,
+      avatar: "https://via.placeholder.com/150",
+      name: "Kim Young Jou",
+      country: "Seoul, South Korea",
+      comment:
+        "This helped me connect with creative people for such a lower price. Such a great app.",
+    },
+    {
+      id: 4,
+      avatar: "https://via.placeholder.com/150",
+      name: "Samhitha",
+      country: "C",
       comment:
         "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     },
     {
-        id: 3,
-        avatar: "https://via.placeholder.com/150",
-        name: "Manasa",
-        country: "U",
-        comment:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      },
-      {
-        id: 4,
-        avatar: "https://via.placeholder.com/150",
-        name: "Samhitha",
-        country: "C",
-        comment:
-          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      },
-      {
-        id: 5,
-        avatar: "https://via.placeholder.com/150",
-        name: "Sanjana",
-        country: "t",
-        comment:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      },
-      {
-        id: 6,
-        avatar: "https://via.placeholder.com/150",
-        name: "Sweety",
-        country: "a",
-        comment:
-          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      },
+      id: 5,
+      avatar: "https://via.placeholder.com/150",
+      name: "Sanjana",
+      country: "t",
+      comment:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      id: 6,
+      avatar: "https://via.placeholder.com/150",
+      name: "Sweety",
+      country: "a",
+      comment:
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
   ];
   const handlePrev = () => {
     if (startIndex === 0) return; // Already at the beginning
     setStartIndex(startIndex - 1);
+    setIsPreviousPressed((prev) => prev - 1);
   };
 
   const handleNext = () => {
-    if (startIndex + 2 >= testimonials.length) return; // Already at the end
-    setStartIndex(startIndex + 1);
+    // if (startIndex + 2 >= testimonials.length) return; // Already at the end
+    // setStartIndex(startIndex + 1);
+    setIsNextPressed((prev) => prev + 1);
   };
 
   return (
-    <div className="flex flex-col">
-      <div className="flex transition-transform duration-1000 delay-700 ease-in-out">
-        {testimonials.slice(startIndex, startIndex + 3).map((testimonial, index) => (
-          <Testimonial
-            key={index}
-            avatar={testimonial.avatar}
-            name={testimonial.name}
-            country={testimonial.country}
-            comment={testimonial.comment}
-          />
-        ))}
+    <section id="testimonials">
+      <h2 className="w-full text-5xl text-center font-bold mt-32 mb-6">
+        Trusted by Thousands of <br />
+        Happy Customer
+      </h2>
+      <h3 className="w-full text-xl text-center mb-16">
+        These are the stories of our customers who have joined us with great{" "}
+        <br />
+        pleasure when using this crazy feature.
+      </h3>
+      <div className="flex flex-col">
+        <div className="flex  duration-1000 delay-700 ease-in-out">
+          {testimonials.map((testimonial, index) => (
+            <Testimonial
+              key={index}
+              avatar={testimonial.avatar}
+              name={testimonial.name}
+              country={testimonial.country}
+              comment={testimonial.comment}
+              isNextPressed={isNextPressed}
+              isPreviousPressed={isPreviousPressed}
+            />
+          ))}
+        </div>
+        <div className="flex justify-end mt-4 mr-16">
+          <button
+            className="w-10 h-10 rounded-full flex items-center justify-center border-2 border-blue-700 hover:bg-blue-700 focus:outline-none"
+            onClick={handlePrev}
+          >
+            <img
+              src={process.env.PUBLIC_URL + "/leftarrow.png"}
+              alt="My Image"
+            />
+          </button>
+          <button
+            className="w-10 h-10 rounded-full flex items-center justify-center border-2 border-blue-700 hover:bg-blue-700 focus:outline-none"
+            onClick={handleNext}
+          >
+            <img
+              src={process.env.PUBLIC_URL + "/rightfill.png"}
+              alt="My Image"
+            />
+          </button>
+        </div>
       </div>
-      <div className="flex justify-end mt-4 mr-16">
-        <button
-          className="w-10 h-10 rounded-full flex items-center justify-center border-2 border-blue-700 hover:bg-blue-700 focus:outline-none"
-          onClick={handlePrev}
-        >
-          <img src={process.env.PUBLIC_URL + '/leftarrow.png'} alt="My Image" />
-        </button>
-        <button
-          className="w-10 h-10 rounded-full flex items-center justify-center border-2 border-blue-700 hover:bg-blue-700 focus:outline-none"
-          onClick={handleNext}
-        >
-          <img src={process.env.PUBLIC_URL + '/rightfill.png'} alt="My Image" />
-        </button>
-      </div>
-    </div>
+    </section>
   );
 };
 
